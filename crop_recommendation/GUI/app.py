164bd2b5117613f262725becb81d4681ffa8d0a4
raphael08 <rsiphael@gmail.com>
2023-01-04 11:50:28 +0300

    GUI

--- crop_recommendation/GUI/app.py
+++ crop_recommendation/GUI/app.py
@@ -8,7 +8,7 @@ import pycaret.clustering as pc
 # print(input_text)
 col1, col2, col3,col4 = st.columns(4)
 col5, col6, col7 = st.columns(3)
-data = pd.read_csv('kmeans_result2.csv')
+data = pd.read_csv('../dataset/kmeans_result2.csv')
 n = float(col1.number_input('enter value of Nitrogen:',key='n', min_value=round(data['N'].min()), max_value=round(data['N'].max())))
 p = float(col2.number_input('enter value of Phorphorus:', key='p', min_value=round(data['P'].min()), max_value=round(data['P'].max())))
 k = float(col3.number_input('enter value of Potassium:', key='k', min_value=round(data['K'].min()), max_value=round(data['K'].max())))
@@ -20,9 +20,9 @@ humidity = float(col7.number_input('enter humidity value:', key='humidity', min_
 
 def process(n,p,k,rainfall,humidity,temperature,ph):
     unknown_data = pd.DataFrame([{'N':round(n),'P':round(p),'K':round(k),'rainfall':round(rainfall),'humidity':round(humidity),'ph':round(ph),'temperature':round(temperature)}])
-    kmeans_result2 = pd.read_csv('kmeans_result2.csv')
+    kmeans_result2 = pd.read_csv('../dataset/kmeans_result2.csv')
     kmeans_result2['Cluster'] = kmeans_result2['Cluster'].str.replace("Cluster",'').apply(int)
-    saved_model = pc.load_model("group2-model")
+    saved_model = pc.load_model("../model/crop-model")
     pred = pc.predict_model(saved_model,unknown_data)
     pred = int(pred['Cluster'][0][-1])
     kmeans_result=kmeans_result2[kmeans_result2['Cluster']==pred]
